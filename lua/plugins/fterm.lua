7e96591be90ba45691a0c8c0f43680696ed0c77e
Marcus Gasberg <guest-maga@systematic.com>
2023-01-24 13:09:37 +0100

    Rebind keys

--- lua/plugins/fterm.lua
+++
@@ -1,6 +0,0 @@
-local fTerm = require("FTerm")
-fTerm.setup({
-  cmd = "powershell"
-})
-vim.keymap.set("n", "<C-z>", fTerm.toggle, {})
-vim.keymap.set("t", "<C-z>", '<C-\\><C-n>:lua require("FTerm").toggle()<CR>')
