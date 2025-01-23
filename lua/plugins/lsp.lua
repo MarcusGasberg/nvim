0b5f3c90ae4c9e17fb9586082f45a04b8f468d7f
Marcus Gasberg <maga@systematic.com>
2025-01-23 10:56:23 +0100

    More changes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,59 +1,55 @@
 return {
-  -- {
-  --   'saghen/blink.cmp',
-  --   -- optional: provides snippets for the snippet source
-  --   dependencies = { 'rafamadriz/friendly-snippets' },
-  --
-  --   version = "v0.9.0",
-  --   -- use a release tag to download pre-built binaries
-  --   -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  --   -- build = 'cargo build --release',
-  --   -- If you use nix, you can build from source using latest nightly rust with:
-  --   -- build = 'nix run .#build-plugin',
-  --
-  --   ---@module 'blink.cmp'
-  --   ---@type blink.cmp.Config
-  --   opts = function()
-  --     return {
-  --       -- 'default' for mappings similar to built-in completion
-  --       -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-  --       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-  --       -- See the full "keymap" documentation for information on defining your own keymap.
-  --       keymap = {
-  --         ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-  --         ['<C-e>'] = { 'hide' },
-  --
-  --         ['<CR>'] = { 'accept', 'fallback' },
-  --
-  --         ['<S-Tab>'] = { 'select_prev', 'fallback' },
-  --         ['<Tab>'] = { 'select_next', 'fallback' },
-  --
-  --         ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-  --         ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-  --       },
-  --
-  --       appearance = {
-  --         -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-  --         -- Useful for when your theme doesn't support blink.cmp
-  --         -- Will be removed in a future release
-  --         use_nvim_cmp_as_default = true,
-  --         -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-  --         -- Adjusts spacing to ensure icons are aligned
-  --         nerd_font_variant = 'mono'
-  --       },
-  --
-  --       -- Default list of enabled providers defined so that you can extend it
-  --       -- elsewhere in your config, without redefining it, due to `opts_extend`
-  --       sources = {
-  --         default = { 'lsp', 'path', 'snippets', 'buffer' },
-  --       },
-  --       completion = {
-  --         list = { selection = 'auto_insert' },
-  --       }
-  --     }
-  --   end,
-  --   opts_extend = { "sources.default" }
-  -- },
+  {
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = { 'rafamadriz/friendly-snippets' },
+
+    -- use a release tag to download pre-built binaries
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    build        = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts         = {
+      -- 'default' for mappings similar to built-in completion
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- See the full "keymap" documentation for information on defining your own keymap.
+      keymap = {
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'hide' },
+
+        ['<CR>'] = { 'accept', 'fallback' },
+
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+        ['<Tab>'] = { 'select_next', 'fallback' },
+
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+      },
+
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- Will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+      completion = {
+        list = { selection = { auto_insert = true } },
+      }
+    }
+  },
   {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -73,7 +69,7 @@ return {
           vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", buffer_opts)
           vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", buffer_opts)
           vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", buffer_opts)
-          vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", buffer_opts)
+          vim.keymap.set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<cr>", buffer_opts)
           vim.keymap.set(
             "n",
             "<leader>vd",
@@ -81,7 +77,6 @@ return {
             { desc = "View Diagnostics" }
           )
           vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>", buffer_opts)
-          vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", buffer_opts)
           vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", buffer_opts)
         end,
       })
@@ -120,82 +115,5 @@ return {
         lspconfig[server].setup(config)
       end
     end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    -- these dependencies will only be loaded when cmp loads
-    -- dependencies are always lazy-loaded unless specified otherwise
-    opts = function(_, opts)
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, {
-        name = "lazydev",
-        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-      })
-    end,
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "onsails/lspkind-nvim",
-      "L3MON4D3/LuaSnip",
-    },
-    cond = not vim.g.vscode,
-    config = function()
-      local cmp = require("cmp");
-      local cmp_select = { behavior = cmp.SelectBehavior.Insert }
-
-
-      -- Setup completion engine
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-          ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-        }),
-      })
-
-      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline({ "/", "?" }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
-      })
-
-      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          {
-            name = "cmdline",
-            option = {
-              ignore_cmds = { "Man", "!", "grep", "vimgrep", "Dispatch" },
-            },
-          },
-        }),
-      })
-
-      cmp.setup.filetype({ "sql" }, {
-        sources = {
-          { name = "vim-dadbod-completion" },
-          { name = "buffer" },
-        },
-      })
-    end
   }
 }
