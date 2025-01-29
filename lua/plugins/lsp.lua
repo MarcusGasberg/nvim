d8c94dc32a8a0e84249f411aa536e483d3cd1acd
Marcus Gasberg <maga@systematic.com>
2025-01-29 10:35:18 +0100

    add jdtls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -167,6 +167,29 @@ return {
               rename = nil,
             },
           }),
+        },
+        jdtls = {
+          cmd = {
+            "java",
+            "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+            "-Dosgi.bundles.defaultStartLevel=4",
+            "-Declipse.product=org.eclipse.jdt.ls.core.product",
+            "-Dlog.protocol=true",
+            "-Dlog.level=ALL",
+            "-Xms1g",
+            "--add-modules=ALL-SYSTEM",
+            "--add-opens",
+            "java.base/java.util=ALL-UNNAMED",
+            "--add-opens",
+            "java.base/java.lang=ALL-UNNAMED",
+            "-javaagent:" .. vim.fn.expand("$MASON/share/jdtls/lombok.jar"),
+            "-jar",
+            vim.fn.expand("$MASON/share/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+            "-configuration",
+            vim.fn.expand("$MASON/share/jdtls/config"),
+            "-data",
+            vim.fn.stdpath("data") .. "/lsp_servers/jdtls_workspace_" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+          },
         }
       }
       local server_names = {}
